4a4915fa0af45cfa3fd6c85e94151b05eee14b0d
Lukimucki <82370950+Lukimucki@users.noreply.github.com>
2021-04-12 10:33:38 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,22 @@
+basic.showNumber(1)
+basic.showLeds(`
+    . . . . .
+    . . # . .
+    . # # # .
+    . . # . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showNumber(1)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . . . . .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showNumber(2)
+basic.forever(function () {
+	
+})
